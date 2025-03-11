a80d452577e2bfbfe5e5d6598d83203aad82b7ac
sivasaiE <shivasai0369@gmail.com>
2025-03-11 21:37:26 +0530

    added another object kumar

--- src/Helper.java
+++ src/Helper.java
@@ -8,6 +8,7 @@ public class Helper {
 
         Student siva = new Student();
         Student sai = new Student();
+        Student Kumar = new Student();
 
 
         siva.name = "Siva";
@@ -17,9 +18,13 @@ public class Helper {
         sai.name = "Sai";
         sai.age = 26;
 
+        Kumar.name = "Kumar";
+        Kumar.age = 26;
+
 
         System.out.println("priting siva name "+ siva.name());
         System.out.println("priting sai name "+ sai.name());
+        System.out.println("priting kumar name "+ Kumar.name());
 
 
     }
