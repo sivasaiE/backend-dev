a4ffbf6773e32ab1cd77fa0be490e54f961bb579
sivasaiE <shivasai0369@gmail.com>
2025-03-11 21:09:07 +0530

    my first commit

---
+++ src/Helper.java
@@ -0,0 +1,26 @@
+public class Helper {
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Student siva = new Student();
+        Student sai = new Student();
+
+
+        siva.name = "Siva";
+        siva.age = 26;
+
+
+        sai.name = "Sai";
+        sai.age = 26;
+
+
+        System.out.println("priting siva name "+ siva.name());
+        System.out.println("priting sai name "+ sai.name());
+
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public  class Student{
+     String name;
+     int age;
+
+
+     public int display(String[] args) {
+         return age;
+
+
+     }
+
+     public String name() {
+         return name;
+     }
+
+
+
+
+}
