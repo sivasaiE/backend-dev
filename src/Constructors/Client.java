8c6bc2e2fe2d1d57385bf35cda0ed0274b1dfbb2
sivasaiE <shivasai0369@gmail.com>
2025-03-12 22:29:08 +0530

    created a refernece object and get its memory location

--- src/Constructors/Client.java
+++ src/Constructors/Client.java
@@ -12,13 +12,24 @@ public class Client {
         System.out.println(user1.age);
         System.out.println(user1.name);
 
+        // This will print the memory location of the object
+        System.out.println(user1);
+
+
+
+
 
         // This is a copy constructor  object  stores its memory in heap memory and the values are copied from user1
         User user2 = new User(user1);
+        // if we change the user2 object it will not affect the user1 object
 
+
+        User user3 = user1;
         // This is a shallow copy and it just points to same memeory location and doesn't create a new object
         // new keyword is used to create objects
-        // user1 = user2
+
+        System.out.println(user3); // This obj location will be same as user1
+
     }
 
 }
